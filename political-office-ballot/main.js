49608916196ff297f7679a6e1b08213d16b1da60
Derek <derekdenhartigh@gmail.com>
2019-03-31 14:09:00 -0400

    parsed JSON file so that I have isolated the office being run for, candidates running, and their political affiliations.

--- political-office-ballot/main.js
+++ political-office-ballot/main.js
@@ -2,16 +2,30 @@
 
 $(()=>{
     $.get("http://michiganelections.io/api/positions/?active_election=false&election_id=&format=json&precinct_county=&precinct_id=1217&precinct_jurisdiction=&precinct_number=&precinct_ward=", (data)=>{
-
+        console.log(data);
         $.each(data.results, function (n){
-            let propNumber = data.results[n].name;
-            let propDescriptionArray = data.results[n].description.split("\n");
-            let propSummary = propDescriptionArray.shift();
+            let child = $(this)[n];  // right now this is just the first child of data..?
+            let office = $(this)[n].name; // Political office being run for
+            let candidateArray = $(this)[n].candidates; // returns array of objects, length 10, (5 undefined), each child contains .name .party
+            let candidateNameArray = [];  // array of candidate names
+            let candidatePartyArray = []; // corresponding array of candidate party affiliation
+                candidateArray.forEach(child => {
+                    if (child!==undefined){  // filters out undefined children from the API
+                        candidateNameArray.push(child.name);
+                        candidatePartyArray.push(child.party.name);
+                    }
+                });
+            
 
-            $("#flex-columns").append(`<div class="prop-box" id="prop${n}-box"></div>`);
-            $(`#prop${n}-box`).append(`<h1 class="prop-title" id="prop${n}-title">Proposal ${propNumber}</h1><br>`);
-            $(`#prop${n}-box`).append(`<h1 class="prop-description" id="prop${n}-description">${propSummary}</h1>`);
-            $(`#prop${n}-box`).append(`<p class="prop-explanation" id="prop${n}-explanation">${propDescriptionArray.join("\n")}</p>`);
+            console.warn(child);
+            console.log(office);  // good
+            console.warn(candidateArray);
+            console.log(candidateNameArray);
+            console.warn(candidatePartyArray);
+            // $("#flex-columns").append(`<div class="prop-box" id="prop${n}-box"></div>`);
+            // $(`#prop${n}-box`).append(`<h1 class="prop-title" id="prop${n}-title">Proposal ${propNumber}</h1><br>`);
+            // $(`#prop${n}-box`).append(`<h1 class="prop-description" id="prop${n}-description">${propSummary}</h1>`);
+            // $(`#prop${n}-box`).append(`<p class="prop-explanation" id="prop${n}-explanation">${propDescriptionArray.join("\n")}</p>`);
         });
     })
 });
