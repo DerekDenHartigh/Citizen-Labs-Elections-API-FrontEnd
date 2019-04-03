b52258ea9fe537ec33a0a1d4993527d1fdf0a10c
Derek <derekdenhartigh@gmail.com>
2019-04-02 20:39:15 -0400

    column layout is looking a lot more like the ballot itself

--- proposal-ballot/main.js
+++ proposal-ballot/main.js
@@ -8,10 +8,14 @@ $(()=>{
             let propDescriptionArray = data.results[n].description.split("\n");
             let propSummary = propDescriptionArray.shift();
 
-            $("#flex-columns").append(`<div class="prop-box" id="prop${n}-box"></div>`);
-            $(`#prop${n}-box`).append(`<h1 class="prop-title" id="prop${n}-title">Proposal ${propNumber}</h1><br>`);
-            $(`#prop${n}-box`).append(`<h1 class="prop-description" id="prop${n}-description">${propSummary}</h1>`);
-            $(`#prop${n}-box`).append(`<p class="prop-explanation" id="prop${n}-explanation">${propDescriptionArray.join("\n")}</p>`);
+            // $("#columns").append(`<div class="prop-box" id="prop${n}-box"></div>`);
+            $("#columns").append(`<br>`);
+            $("#columns").append(`<h1 class="prop-title" id="prop${n}-title">Proposal ${propNumber}</h1><br>`);
+            $("#columns").append(`<h1 class="prop-description padding" id="prop${n}-description">${propSummary}</h1>`);
+            $("#columns").append(`<p class="prop-explanation padding" id="prop${n}-explanation">${propDescriptionArray.join("\n")}</p>`);
+            $("#columns").append(`<br><div class="flex-wrapper"><h2 class="yes-no">YES   <div class="oval"></p></h2></div>`);
+            $("#columns").append(`<div class="flex-wrapper"><h2 class="yes-no">NO   <div class="oval"></p></h2></div>`);
+            $("#columns").append(`<hr class="line-break">`);
         });
     })
 });
